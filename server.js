d7ef61ca26a93cb92660b2ecf3563c5c94b0790f
Surya H R <101381585+SuryaHR@users.noreply.github.com>
2023-02-24 16:07:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,3 +44,22 @@ app.post('/store-goal', (req, res) => {
 });
 
 app.listen(8080);
+
+//'use strict';
+
+// const express = require('express');
+
+// // Constants
+// const PORT = 8080;
+// const HOST = '0.0.0.0';
+
+// // App
+// const app = express();
+// app.get('/', (req, res) => {
+//   res.send('Hello World');
+// });
+
+// app.listen(PORT, HOST, () => {
+//   console.log(`Running on http://${HOST}:${PORT}`);
+// });
+
